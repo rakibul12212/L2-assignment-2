e90e205424b129f2deb2d4f7fc752fdafb7eb17d
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-12-08 12:45:53 -0800

    install ts node dev

--- dist/server.js
+++ dist/server.js
@@ -13,14 +13,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const config_1 = __importDefault(require("./app/config"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = __importDefault(require("./app/config"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(config_1.default.database_url);
             app_1.default.listen(config_1.default.port, () => {
-                console.log(`app is listening on port$(config.port)`);
+                console.log(`App listening on port ${config_1.default.port}`);
             });
         }
         catch (err) {
